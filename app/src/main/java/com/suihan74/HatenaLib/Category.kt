0c8bc83b8cd85979e2f1f19cb4863ec9fa2d2464
suihan <suihan742@gmail.com>
2019-11-06 02:47:12 +0900

    added 'general' category

--- app/src/main/java/com/suihan74/HatenaLib/Category.kt
+++ app/src/main/java/com/suihan74/HatenaLib/Category.kt
@@ -1,21 +1,22 @@
 package com.suihan74.HatenaLib
 
-enum class Category(
+enum class Category (
     val int: Int,
     val code: String
 ) {
     All(0, "315767106563433873"),
-    Social(1, "301816409282464093"),
-    Economics(2, "300989576564947867"),
-    Life(3, "244148959988020477"),
-    Knowledge(4, "315890158150969179"),
-    It(5, "261248828312298389"),
-    Entertainment(6, "302115476501939948"),
-    Game(7, "297347994088281699"),
-    Fun(8, "302115476506048236");
-//    CurrentEvents(9, "83497569613451046"),
+    General(1, "315756341902288872"),
+    Social(2, "301816409282464093"),
+    Economics(3, "300989576564947867"),
+    Life(4, "244148959988020477"),
+    Knowledge(5, "315890158150969179"),
+    It(6, "261248828312298389"),
+    Entertainment(7, "302115476501939948"),
+    Game(8, "297347994088281699"),
+    Fun(9, "302115476506048236");
+//    CurrentEvents(10, "83497569613451046"),
 
     companion object {
-        fun fromInt(i: Int) : Category = values().getOrNull(i) ?: All
+        fun fromInt(i: Int) : Category = values().firstOrNull { it.int == i } ?: All
     }
 }
--- app/src/main/java/com/suihan74/satena/models/PreferenceKey.kt
+++ app/src/main/java/com/suihan74/satena/models/PreferenceKey.kt
@@ -1,12 +1,13 @@
 package com.suihan74.satena.models
 
+import android.content.Context
 import com.suihan74.utilities.SafeSharedPreferences
 import com.suihan74.utilities.SharedPreferencesKey
 import com.suihan74.utilities.typeInfo
 import org.threeten.bp.LocalDateTime
 import java.lang.reflect.Type
 
-@SharedPreferencesKey(fileName = "default", version = 1, latest = true)
+@SharedPreferencesKey(fileName = "default", version = 2, latest = true)
 enum class PreferenceKey(
     override val valueType: Type,
     override val defaultValue: Any?
@@ -63,3 +64,35 @@ enum class PreferenceKey(
     CUSTOM_BOOKMARKS_IS_MUTED_USERS_ACTIVE(typeInfo<Boolean>(), false)
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// previous versions
+////////////////////////////////////////////////////////////////////////////////
+
+/**************************************
+ * version 1 -> 2 での変更
+ * Category.GeneralをCategory.AllとCategory.Socialの間に挿入したことによる変更を
+ * ENTRIES_HOME_CATEGORYにも適用するための変更
+ **************************************/
+
+////////////////////////////////////////////////////////////////////////////////
+// version migration
+////////////////////////////////////////////////////////////////////////////////
+
+object PreferenceKeyMigrator {
+    fun check(context: Context) {
+        val version = SafeSharedPreferences.version<IgnoredEntriesKey>(context)
+        when (version) {
+            1 -> migrateFromVersion1(context)
+        }
+    }
+
+    private fun migrateFromVersion1(context: Context) {
+        val prefs = SafeSharedPreferences.create<PreferenceKey>(context)
+        val homeCategory = prefs.getInt(PreferenceKey.ENTRIES_HOME_CATEGORY)
+        prefs.edit {
+            if (homeCategory > 0) {
+                putInt(PreferenceKey.ENTRIES_HOME_CATEGORY, homeCategory + 1)
+            }
+        }
+    }
+}
