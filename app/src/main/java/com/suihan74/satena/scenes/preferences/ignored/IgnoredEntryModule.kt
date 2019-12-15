d42cc96a7416019e3751f9327a84677cd6218077
suihan <suihan742@gmail.com>
2019-12-16 04:07:52 +0900

    divided files related to IgnoredEntryViewModel

---
+++ app/src/main/java/com/suihan74/satena/scenes/preferences/ignored/IgnoredEntryModule.kt
@@ -0,0 +1,16 @@
+package com.suihan74.satena.scenes.preferences.ignored
+
+import com.suihan74.satena.SatenaApplication
+import dagger.Module
+import dagger.Provides
+
+@Module
+class IgnoredEntryModule(private val app: SatenaApplication) {
+    @Provides
+    fun provideIgnoredEntryDao() =
+        app.ignoredEntryDao
+
+    @Provides
+    fun provideIgnoredEntryRepository() =
+        IgnoredEntryRepository()
+}
--- app/src/main/java/com/suihan74/satena/scenes/preferences/ignored/IgnoredEntryViewModel.kt
+++ app/src/main/java/com/suihan74/satena/scenes/preferences/ignored/IgnoredEntryViewModel.kt
@@ -1,74 +1,15 @@
 package com.suihan74.satena.scenes.preferences.ignored
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.suihan74.satena.SatenaApplication
 import com.suihan74.satena.models.ignoredEntry.IgnoredEntry
-import com.suihan74.satena.models.ignoredEntry.IgnoredEntryDao
-import dagger.Module
-import dagger.Provides
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-@Module
-class IgnoredEntryModule(private val app: SatenaApplication) {
-    @Provides
-    fun provideIgnoredEntryDao() =
-        app.ignoredEntryDao
-
-    @Provides
-    fun provideIgnoredEntryRepository() =
-        IgnoredEntryRepository()
-}
-
-class IgnoredEntryRepository {
-    @Inject lateinit var dao: IgnoredEntryDao
-
-    suspend fun load() = withContext(Dispatchers.IO) {
-        return@withContext dao.getAllEntries()
-    }
-
-    suspend fun add(entry: IgnoredEntry) : IgnoredEntry? = withContext(Dispatchers.IO) {
-        return@withContext try {
-            dao.insert(entry)
-            dao.find(entry.type, entry.query)
-        }
-        catch (e: Exception) {
-            Log.e("IgnoredEntryVM", "the entry is duplicated")
-            null
-        }
-    }
-
-    suspend fun delete(entry: IgnoredEntry) = withContext(Dispatchers.IO) {
-        try {
-            dao.delete(entry)
-            return@withContext
-        }
-        catch (e: Exception) {
-            Log.e("IgnoredEntryVM", "the entry does not exist")
-        }
-    }
-
-    suspend fun update(modified: IgnoredEntry) = withContext(Dispatchers.IO) {
-        try {
-            dao.update(modified)
-        }
-        catch (e: Exception) {
-            Log.e("IgnoredEntryVM", "failed to update the entry")
-        }
-    }
-
-    init {
-        SatenaApplication.instance.component.inject(this)
-    }
-}
 
 class IgnoredEntryViewModel : ViewModel() {
-
     @Inject lateinit var repository: IgnoredEntryRepository
 
     val entries: MutableLiveData<List<IgnoredEntry>> by lazy {
