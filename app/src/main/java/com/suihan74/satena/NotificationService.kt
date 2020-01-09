b8b5ca49e6d5c9eac0d2d575f8173c13046fc6d5
suihan <suihan742@gmail.com>
2020-01-09 20:17:51 +0900

    replace .bookmarks with .bookmarks2 on NotificationService

--- app/src/main/java/com/suihan74/satena/NotificationService.kt
+++ app/src/main/java/com/suihan74/satena/NotificationService.kt
@@ -12,7 +12,7 @@ import androidx.core.app.NotificationManagerCompat
 import com.suihan74.HatenaLib.HatenaClient
 import com.suihan74.HatenaLib.Notice
 import com.suihan74.satena.models.PreferenceKey
-import com.suihan74.satena.scenes.bookmarks.BookmarksActivity
+import com.suihan74.satena.scenes.bookmarks2.BookmarksActivity
 import com.suihan74.satena.scenes.entries.EntriesActivity
 import com.suihan74.satena.scenes.entries.notices.message
 import com.suihan74.utilities.AccountLoader
