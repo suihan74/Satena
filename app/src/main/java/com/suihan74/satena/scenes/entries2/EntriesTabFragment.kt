9abca8d4996989e815a9c649ed54092478f3378a
suihan <suihan742@gmail.com>
2020-03-27 22:16:26 +0900

    fix Category.Site

--- app/src/main/java/com/suihan74/satena/scenes/entries2/EntriesTabFragment.kt
+++ app/src/main/java/com/suihan74/satena/scenes/entries2/EntriesTabFragment.kt
@@ -110,13 +110,7 @@ class EntriesTabFragment : EntriesTabFragmentBase() {
         })
 
         // SiteUrlを監視する
-        var isSiteUrlInitialized = false
         parentViewModel.siteUrl.observe(viewLifecycleOwner, Observer {
-            if (!isSiteUrlInitialized) {
-                isSiteUrlInitialized = true
-                return@Observer
-            }
-
             viewModel.siteUrl = it
             viewModel.refresh(onErrorRefreshEntries)
         })
