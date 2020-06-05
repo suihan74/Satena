a4ceae831ab9b203f13121e6eea99eaa98660ad2
suihan <suihan742@gmail.com>
2020-06-05 21:52:02 +0900

    fix inheritance

--- app/src/main/java/com/suihan74/satena/scenes/preferences/PreferencesViewModel.kt
+++ app/src/main/java/com/suihan74/satena/scenes/preferences/PreferencesViewModel.kt
@@ -1,12 +1,13 @@
 package com.suihan74.satena.scenes.preferences
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.suihan74.satena.models.PreferenceKey
 import com.suihan74.utilities.SafeSharedPreferences
 
 abstract class PreferencesViewModel(
     val prefs: SafeSharedPreferences<PreferenceKey>
-) {
+) : ViewModel() {
     /** SafeSharedPreferencesと紐づいたLiveDataを作成する */
     protected inline fun <reified T> createLiveData(key: PreferenceKey) =
         MutableLiveData<T>(prefs.get<T>(key)).apply {
