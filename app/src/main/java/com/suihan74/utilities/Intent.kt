53771ad53b744cfc4d2a046c5abcfe912d6d9d88
suihan <suihan742@gmail.com>
2020-07-15 23:28:38 +0900

    add a extension for sharing URL by excluding this application itself from candidates

---
+++ app/src/main/java/com/suihan74/utilities/Intent.kt
@@ -0,0 +1,27 @@
+package com.suihan74.utilities
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+
+/** URLを開くために"共有先リストからこのアプリを除いた"Intentを作成する */
+fun Intent.openUrlExcludeApplication(context: Context) : Intent {
+    val packageManager = context.packageManager
+    val dummyIntent = Intent(this.action, Uri.parse("https://dummy"))
+    val intentActivities = packageManager.queryIntentActivities(dummyIntent, PackageManager.MATCH_ALL)
+        .plus(packageManager.queryIntentActivities(this, PackageManager.MATCH_ALL))
+        .distinctBy { it.activityInfo.name }
+
+    val intents = intentActivities
+        .filterNot { it.activityInfo.packageName == context.packageName }
+        .map { Intent(this).apply { setPackage(it.activityInfo.packageName) } }
+
+    return when (intents.size) {
+        0 -> this
+        1 -> intents.first()
+        else -> Intent.createChooser(Intent(), "Choose a browser").apply {
+            putExtra(Intent.EXTRA_INITIAL_INTENTS, intents.toTypedArray())
+        }
+    }
+}
