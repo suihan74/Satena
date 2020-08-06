dd207c48fb52b784be24bbff30859fa7d848176e
suihan <suihan742@gmail.com>
2020-08-06 22:38:06 +0900

    fix HatenaClient.getEntryUrlFromCommentPageUrl to be able to handle entry?eid=~~~ pattern

---
+++ app/src/androidTest/java/com/suihan74/satena/HatenaClientTest.kt
@@ -0,0 +1,30 @@
+package com.suihan74.satena
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.suihan74.hatenaLib.HatenaClient
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class HatenaClientTest {
+    @Test
+    fun getEntryUrl_A() {
+        val url0 = HatenaClient.getEntryUrlFromCommentPageUrl("https://b.hatena.ne.jp/entry/12345678")
+        val url1 = HatenaClient.getEntryUrlFromCommentPageUrl("https://b.hatena.ne.jp/entry?eid=12345678")
+        val url2 = HatenaClient.getEntryUrlFromCommentPageUrl("https://b.hatena.ne.jp/entry/12345678/comment/suihan74")
+
+        assertEquals("url0 != url1", url0, url1)
+        assertEquals("url0 != url2", url0, url2)
+    }
+
+    @Test
+    fun getEntryUrl_B() {
+        val url0 = HatenaClient.getEntryUrlFromCommentPageUrl("https://b.hatena.ne.jp/entry?url=https://suihan74.github.io/")
+        val url1 = HatenaClient.getEntryUrlFromCommentPageUrl("https://b.hatena.ne.jp/entry/https://suihan74.github.io/")
+        val url2 = HatenaClient.getEntryUrlFromCommentPageUrl("https://b.hatena.ne.jp/entry/s/suihan74.github.io/")
+
+        assertEquals("url0 != url1", url0, url1)
+        assertEquals("url0 != url2", url0, url2)
+    }
+}
