a93859df372678c707cb02213035adfb6ccc9252
suihan <suihan742@gmail.com>
2020-09-15 12:36:53 +0900

    add extensions for boolean

---
+++ app/src/main/java/com/suihan74/utilities/Boolean.kt
@@ -0,0 +1,24 @@
+@file:Suppress("unused")
+
+package com.suihan74.utilities
+
+import android.view.View
+
+/** trueのときView.VISIBLEにする */
+fun Boolean?.toVisibility(defaultInvisible: Int = View.GONE) : Int =
+    if (this == true) View.VISIBLE
+    else defaultInvisible
+
+/** trueのときに処理を実行する */
+inline fun Boolean?.whenTrue(crossinline action: ()->Unit) : Boolean {
+    val result = this == true
+    if (result) action.invoke()
+    return result
+}
+
+/** falseのときに処理を実行する */
+inline fun Boolean?.whenFalse(crossinline action: ()->Unit) : Boolean {
+    val result = this == true
+    if (!result) action.invoke()
+    return result
+}
--- app/src/main/java/com/suihan74/utilities/BooleanVisibilityConverter.kt
+++
@@ -1,7 +0,0 @@
-package com.suihan74.utilities
-
-import android.view.View
-
-fun Boolean?.toVisibility(defaultInvisible: Int = View.GONE) =
-    if (this == true) View.VISIBLE
-    else defaultInvisible
