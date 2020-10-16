f1cd1e83a357d0d63eb2d5b9f6ebb8e839fe7a32
suihan <suihan742@gmail.com>
2020-10-16 21:55:23 +0900

    replace Glide with GlideApp

---
+++ app/src/main/java/com/suihan74/satena/SatenaGlideModule.kt
@@ -0,0 +1,23 @@
+package com.suihan74.satena
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
+
+@GlideModule
+class SatenaGlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.apply {
+            RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .signature(
+                    ObjectKey(System.currentTimeMillis().toShort())
+                )
+        }
+    }
+}
--- app/src/main/java/com/suihan74/utilities/bindings/ImageView.kt
+++ app/src/main/java/com/suihan74/utilities/bindings/ImageView.kt
@@ -2,16 +2,17 @@ package com.suihan74.utilities.bindings
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
 import com.suihan74.hatenaLib.HatenaClient
 import com.suihan74.hatenaLib.Notice
+import com.suihan74.satena.GlideApp
 import com.suihan74.utilities.extensions.users
 
 /** URL先の画像をImageViewで表示 */
 @BindingAdapter("src")
 fun ImageView.setSource(url: String?) {
-    if (url != null) {
-        Glide.with(context)
+    val context = context ?: return
+    if (!url.isNullOrBlank()) {
+        GlideApp.with(context)
             .load(url)
             .into(this)
     }
