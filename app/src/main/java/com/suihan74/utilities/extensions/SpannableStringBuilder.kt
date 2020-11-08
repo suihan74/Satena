20fe75bda1a2a324e3578cc747a54770262dd727
suihan <suihan742@gmail.com>
2020-11-08 11:52:07 +0900

    add SpannableStringBuilder#appendDrawable

--- app/src/main/java/com/suihan74/utilities/extensions/SpannableStringBuilder.kt
+++ app/src/main/java/com/suihan74/utilities/extensions/SpannableStringBuilder.kt
@@ -1,7 +1,13 @@
 package com.suihan74.utilities.extensions
 
+import android.content.Context
+import android.os.Build
 import android.text.Spannable
 import android.text.SpannableStringBuilder
+import android.text.style.ImageSpan
+import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 
 /**
  * 装飾を施したテキストを追加する
@@ -28,4 +34,55 @@ fun SpannableStringBuilder.append(
         append(text)
         setSpan(span, startPos, endPos, flags)
     }
+}
+
+/**
+ * ```SpannableStringBuilder```に画像リソースを追加する
+ *
+ * @param verticalAlign 縦方向の位置指定 (省略時、(可能な限り)中央揃え)
+ * - [android.text.style.DynamicDrawableSpan#ALIGN_BASELINE]
+ * - [android.text.style.DynamicDrawableSpan#ALIGN_BOTTOM]
+ * - [android.text.style.DynamicDrawableSpan#ALIGN_CENTER]
+ */
+fun SpannableStringBuilder.appendDrawable(
+    context: Context,
+    @DrawableRes resId: Int,
+    sizePx: Int,
+    color: Int,
+    verticalAlign: Int? = null
+) {
+    ContextCompat.getDrawable(context, resId)?.let { drawable ->
+        drawable.setTint(color)
+        drawable.setBounds(0, 0, sizePx, sizePx)
+
+        val vAlign = verticalAlign ?:
+            if (Build.VERSION.SDK_INT >= 29) ImageSpan.ALIGN_CENTER
+            else ImageSpan.ALIGN_BASELINE
+
+        append("_", ImageSpan(drawable, vAlign))
+    }
+}
+
+/**
+ * ```SpannableStringBuilder```に画像リソースを追加する
+ *
+ * @param verticalAlign 縦方向の位置指定 (省略時、(可能な限り)中央揃え)
+ * - [android.text.style.DynamicDrawableSpan#ALIGN_BASELINE]
+ * - [android.text.style.DynamicDrawableSpan#ALIGN_BOTTOM]
+ * - [android.text.style.DynamicDrawableSpan#ALIGN_CENTER]
+ */
+fun SpannableStringBuilder.appendDrawable(
+    textView: TextView,
+    @DrawableRes resId: Int,
+    sizePx: Int? = null,
+    color: Int? = null,
+    verticalAlign: Int? = null
+) {
+    appendDrawable(
+        context = textView.context,
+        resId = resId,
+        sizePx = sizePx ?: textView.lineHeight,
+        color = color ?: textView.textColors.defaultColor,
+        verticalAlign = verticalAlign
+    )
 }
